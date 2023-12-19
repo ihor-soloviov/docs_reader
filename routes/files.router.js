1eeb36da0558740fd1c526dd9bfac7987e5afa91
Ihor Soloviov <igor.musson.55@gmail.com>
2023-12-19 12:21:49 +0200

    route was added

--- routes/files.router.js
+++ routes/files.router.js
@@ -1,6 +1,7 @@
 const Router = require("express");
 const router = new Router();
 const upload = require("../middleware/uploads");
+const mainPicStor = require("../middleware/picturesSaver");
 const filesController = require("../controllers/files.controller");
 const databaseController = require("../controllers/database.controller");
 
@@ -9,6 +10,9 @@ router.post(
   upload.single("docxFile"),
   filesController.getDataFromDocx
 );
+router.post("/mainImage", mainPicStor.single("mainImage"), (req, res) => {
+  res.send("Файл успішно завантажено");
+});
 router.post("/addRow", databaseController.addRow);
 router.get("/getTable", databaseController.getDataFromTable);
 router.post("/addDataToColumn", databaseController.addDataToColumn);
