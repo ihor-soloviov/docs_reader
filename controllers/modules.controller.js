3ff7982b2d35fa14da0e60f9be826f6b94ac272f
Ihor Soloviov <igor.musson.55@gmail.com>
2024-05-31 18:46:37 +0300

    minor fixe

--- controllers/modules.controller.js
+++ controllers/modules.controller.js
@@ -296,7 +296,7 @@ class ModulesController {
           montage, 
           
         ) VALUES (
-          $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14
+          $1, $2, $3
         )
       `,
         [
