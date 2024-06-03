0e95f509e23877771421b2fd8cf71d5d9fd15da7
Ihor Soloviov <igor.musson.55@gmail.com>
2024-06-03 22:25:45 +0300

    factored angebot scheme

--- src/models/angebots.js
+++ src/models/angebots.js
@@ -2,18 +2,18 @@ const { Schema, model } = require('mongoose');
 const { ServiceSchema } = require('./services'); // Імпорт схеми, а не моделі
 
 const AngebotSchema = new Schema({
-  angebot_id: { type: Number, unique: true, required: true },
-  angebot_type: { type: String, unique: false, required: true },
+  angebotId: { type: Number, unique: true, required: true },
+  angebotType: { type: String, unique: false, required: true },
   montage: { type: [ServiceSchema], unique: false, required: false },
-  under_constructions: { type: [ServiceSchema], unique: false, required: false },
-  pv_module: { type: [ServiceSchema], unique: false, required: false },
-  pvsol_file: { type: [ServiceSchema], unique: false, required: false },
+  underConstructions: { type: [ServiceSchema], unique: false, required: false },
+  pvModule: { type: [ServiceSchema], unique: false, required: false },
+  pvsolFileData: { type: [ServiceSchema], unique: false, required: false },
   invertor: { type: [ServiceSchema], unique: false, required: false },
-  iq_combiner: { type: [ServiceSchema], unique: false, required: false },
+  iqCombiner: { type: [ServiceSchema], unique: false, required: false },
   optimizer: { type: [ServiceSchema], unique: false, required: false },
   battery: { type: [ServiceSchema], unique: false, required: false },
   wallbox: { type: [ServiceSchema], unique: false, required: false },
-  backup_box: { type: [ServiceSchema], unique: false, required: false },
+  backupBox: { type: [ServiceSchema], unique: false, required: false },
   taubenschutz: { type: [ServiceSchema], unique: false, required: false },
   zusatzarbeiten: { type: [ServiceSchema], unique: false, required: false },
 });
