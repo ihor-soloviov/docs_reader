3222c88a3def1bb4f53609b151199feaac2d62e1
Ihor Soloviov <igor.musson.55@gmail.com>
2024-06-03 14:55:41 +0300

    changed case of scheme key

--- src/models/angebots.js
+++ src/models/angebots.js
@@ -2,11 +2,11 @@ const { Schema, model } = require('mongoose');
 const { ServiceSchema } = require('./services'); // Імпорт схеми, а не моделі
 
 const AngebotSchema = new Schema({
-  angebotId: { type: Number, unique: true, required: true },
+  angebot_id: { type: Number, unique: true, required: true },
   angebot_type: { type: String, unique: false, required: true },
   montage: { type: [ServiceSchema], unique: false, required: false },
   under_constructions: { type: [ServiceSchema], unique: false, required: false },
-  pvModule: { type: [ServiceSchema], unique: false, required: false },
+  pv_module: { type: [ServiceSchema], unique: false, required: false },
   pvsol_file: { type: [ServiceSchema], unique: false, required: false },
   invertor: { type: [ServiceSchema], unique: false, required: false },
   iq_combiner: { type: [ServiceSchema], unique: false, required: false },
