289f14b3a46efb2391f15299fd04e3d9ed0242f8
Ihor Soloviov <igor.musson.55@gmail.com>
2024-06-03 22:36:24 +0300

    minor fix

--- src/models/pvsolFileItem.js
+++ src/models/pvsolFileItem.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const PvsolFileItemScheme = new Schema({
   title: { type: String, unique: false, required: true },
-  price: { type: String, unique: false, required: true },
+  number: { type: String, unique: false, required: true },
   measurement: { type: String, unique: false, required: true },
 }, { strict: false });
 
