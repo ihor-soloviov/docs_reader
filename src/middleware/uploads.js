5b52ea2691495d80d433e78443b3f34383cc48ee
Ihor Soloviov <igor.musson.55@gmail.com>
2024-06-03 21:41:27 +0300

    test2

--- src/middleware/uploads.js
+++ src/middleware/uploads.js
@@ -1,16 +1,20 @@
 const multer = require("multer");
 const path = require("path");
 
+const uploadPath = path.resolve(__dirname, "../../uploads/uploadsDocx");
+
+// Перевіряємо, чи існує папка, якщо ні - створюємо її
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+
 const storage = multer.diskStorage({
-
   destination(req, file, cb) {
-    cb(null, '../uploads/uploadsDocx');
+    cb(null, uploadPath);
   },
-
   filename(req, file, cb) {
     cb(null, file.originalname);
   },
-
 });
 
 module.exports = multer({ storage });
